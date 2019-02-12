8a1795a03f5365a06a420881ad0910785008bd02
Ty Mazey <tymazey@gmail.com>
2019-02-11 17:20:05 -0700

    passes test for not allowing users to register with missing info or a non unique email
    <rene.casco@icloud.com>
    <40582072+Vjp888@users.noreply.github.com>
    <421236+n-flint@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
-    session[:user_id] = @user.id
-    redirect_to user_path(@user)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def show
--- spec/features/user_can_register_spec.rb
+++ spec/features/user_can_register_spec.rb
@@ -22,4 +22,42 @@ RSpec.describe 'as visitor', type: :feature do
     expect(current_path).to eq(user_path(user))
     expect(page).to have_content("Welcome #{user.username}!")
   end
+
+  it 'does not allow user to register with missing information' do
+    visit items_path
+
+    click_link 'Register'
+
+    fill_in 'Username', with: 'bob'
+    fill_in 'Street', with: '123 main st'
+    fill_in 'City', with: 'denver'
+    fill_in 'State', with: 'CO'
+    fill_in 'Email', with: 'bob@bob.net'
+    fill_in 'Password', with: 'password'
+    fill_in 'Confirm password', with: 'password'
+    click_on 'Register Now'
+
+    expect(page).to have_content('Username')
+    expect(page).to have_content('Email')
+  end
+
+  it 'does not allow user to register with an email that has already been used' do
+    user = User.create!(username: 'bob', street: '123 main st', city: 'denver', state: 'CO', zip_code: 80216, email: 'bob@bob.net', password: 'password')
+    visit items_path
+
+    click_link 'Register'
+
+    fill_in 'Username', with: 'bobby'
+    fill_in 'Street', with: '123 main st'
+    fill_in 'City', with: 'denver'
+    fill_in 'State', with: 'CO'
+    fill_in 'Zip code', with: 80216
+    fill_in 'Email', with: 'bob@bob.net'
+    fill_in 'Password', with: 'password'
+    fill_in 'Confirm password', with: 'password'
+    click_on 'Register Now'
+
+    expect(page).to have_content('Username')
+    expect(page).to have_content('Email')
+  end
 end
