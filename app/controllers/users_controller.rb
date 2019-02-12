c12dea5669aa874fea0bc19ce90321a51ef036a4
vjp888 <rellik888@gmail.com>
2019-02-11 17:55:56 -0700

    Adds flash messages for missing email and missing information all tests passing
    co-authored-by: Ty Mazey <43197720+TyMazey@users.noreply.github.com>
    co-authored-by: Rene Casco <rene.casco@icloud.com>
    co-authored-by: Noah Flint <421236+n-flint@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    
     if @user.save
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      flash[:error] = "You are missing information"
-      render :new
+      if User.find_by(email: @user.email)
+        flash[:error] = "This E-mail is already registered"
+        render :new
+      else
+        flash[:error] = "You are missing information"
+        render :new
+      end
     end
   end
 
