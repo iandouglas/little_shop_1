1b3a7a28117c552941a19bfe90db90fb5dc63513
Rene Casco <rene.casco@icloud.com>
2019-02-13 14:59:41 -0700

    User Story 25 Visitor can add item to cart
    Co-authored-by: Ty Mazey <43197720+TyMazey@users.noreply.github.com>

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,8 +2,11 @@ class CartsController < ApplicationController
 
   def create
     item = Item.find(params[:item_id])
-    session[:cart] ||= @cart.contents
-
+    item_id_str = item.id.to_s
+    @cart.add_item(item_id_str)
+    session[:cart] = @cart.contents
+    quantity = session[:cart][item_id_str]
+    flash[:success] = "#{item.name} has been succesfully added to your cart."
     redirect_to items_path
   end
 
