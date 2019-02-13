b99510666aeb11e97aee1777d72771e57876f141
vjp888 <rellik888@gmail.com>
2019-02-13 14:19:46 -0700

    Adds redirect for user visiting login page when already logged in
    co-authored-by: Ty Mazey <43197720+TyMazey@users.noreply.github.com>
    co-authored-by: Rene Casco <rene.casco@icloud.com>
    co-authored-by: Noah Flint <421236+n-flint@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,29 @@
 class SessionsController < ApplicationController
 
   def new
-
+    if admin_user?
+      flash[:error] = "You have already logged in"
+      redirect_to root_path
+    elsif merchant_user?
+      flash[:error] = "You have already logged in"
+      redirect_to dashboard_path
+    elsif regular_user?
+      flash[:error] = "You have already logged in"
+      redirect_to profile_path
+    end
   end
 
   def create
+    if admin_user?
+      redirect_to root_path
+      flash[:error] = "You have already logged in"
+    elsif merchant_user?
+      redirect_to dashboard_path
+      flash[:error] = "You have already logged in"
+    elsif regular_user?
+      redirect_to profile_path
+      flash[:error] = "You have already logged in"
+    end
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
