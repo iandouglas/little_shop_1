4197c520d5ee91ec5f17776800d5967e7c86450f
vjp888 <rellik888@gmail.com>
2019-02-14 15:31:28 -0700

    Removes username from the uniqueness check

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe User, type: :model do
     it {should validate_presence_of :zip_code}
     it {should validate_presence_of :email}
     it {should validate_presence_of :password_digest}
-    it {should validate_uniqueness_of :username}
     it {should validate_uniqueness_of :email}
   end
 end
