d4d7c8f943cd2bb99b4825a0c2e18f5ba672af1c
vjp888 <rellik888@gmail.com>
2019-02-14 11:57:58 -0700

    Adds test for navbar, verifies that is is showing in the correct locations
    co-authored-by: Noah Flint <421236+n-flint@users.noreply.github.com>

--- spec/features/a_user_can_see_their_orders_in_showpage_spec.rb
+++ spec/features/a_user_can_see_their_orders_in_showpage_spec.rb
@@ -10,10 +10,21 @@ RSpec.describe 'as a registered user', type: :feature do
       visit profile_path
 
       expect(current_path).to eq(profile_path)
-      expect(page).to have_link('View Orders')
+      within '.user-info' do
+        expect(page).to have_link('View Orders')
+      end
     end
 
-    it 'in the nav a user sees a link to their orders page'
+    it 'in the nav a user sees a link to their orders page' do
+      user = User.create(username: 'bob', street: "1234", city: "bob", state: "bobby", zip_code: 12345, email: "12345@54321", password: "password", role: 0, enabled: 0)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit profile_path
+
+      within '.navbar' do
+        expect(page).to have_link("Orders")
+      end
+    end
 
   end
 
