d00ce4f0fd6112079f736e2235804bcd231978ba
vjp888 <rellik888@gmail.com>
2019-02-14 11:57:58 -0700

    Adds total_item_quantity method to order model test passes
    co-authored-by: Noah Flint <421236+n-flint@users.noreply.github.com>

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,8 @@ class Order < ApplicationRecord
   has_many :items, through: :order_items
 
   enum status: ['pending', 'fufilled', 'cancelled']
+
+  def total_item_quantity
+    order_items.sum(:quantity)
+  end
 end
