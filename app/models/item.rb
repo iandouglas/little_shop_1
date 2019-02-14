421d12db18ace30f31bf99a0028f8c929d3d1933
vjp888 <rellik888@gmail.com>
2019-02-14 11:57:58 -0700

    Adds order_quantity method test passes

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :order_items
   has_many :orders, through: :order_items
+
+  def order_quantity(order_id)
+    OrderItem.find_by(order_id: order_id).quantity
+  end
 end
