014f3ce51eef88de73c1c5b0dfba268e9ada79ec
Noah Flint <noah.flint24@gmail.com>
2019-02-14 15:27:58 -0700

    created model test for OrderItems model

---
+++ spec/models/order_item_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe OrderItems, type: :model do
+
+  describe 'validations' do
+    it {should validate_presence_of :item_id}
+    it {should validate_presence_of :order_id}
+    it {should validate_presence_of :fulfilled}
+    it {should validate_presence_of :current_price}
+    it {should validate_presence_of :quantity}
+  end
+
+  describe 'realationships' do
+    it {should belong_to :item}
+    it {should belong_to :order}
+  end
+end
