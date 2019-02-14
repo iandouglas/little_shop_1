7ba8103fb393f72c314bd1e3dffa95f93b7a034c
vjp888 <rellik888@gmail.com>
2019-02-14 11:57:58 -0700

    Adds test to cancel order

--- spec/features/user_order_cancelation_spec.rb
+++ spec/features/user_order_cancelation_spec.rb
@@ -19,4 +19,25 @@ RSpec.describe 'as a registered user' do
       expect(page).to_not have_button("Cancel Order")
     end
   end
+
+  describe 'when I press the cancel order button' do
+    it 'cancels an order when I press the button' do
+      user = User.create!(username: 'unhappy', street: '432 main st', city: 'steve', state: 'CO', zip_code: 80126, email: '1test@bob.net', password: 'password', role: 0)
+      merchant = User.create(username: 'Jon', street: "1234", city: "Dallas", state: "TX", zip_code: 12345, email: "merchant1@54321", password: "password", role: 1, enabled: 0)
+      order_1 = user.orders.create(user_id: user.id)
+      item_1 = Item.create(name: 'pot_1', description:'a small pot for plants', quantity: 30, price: 2.50, thumbnail: "https://i.etsystatic.com/13875023/r/il/0db0e5/1570825768/il_570xN.1570825768_anvx.jpg", user: merchant)
+      item_2 = Item.create(name: 'pot_2', description:'a small pot for plants', quantity: 20, price: 20.00, thumbnail: "https://i.etsystatic.com/13875023/r/il/0db0e5/1570825768/il_570xN.1570825768_anvx.jpg", user: merchant)
+      OrderItem.create(item_id: item_1.id, order_id: order_1.id, quantity: 3, current_price: 2.50)
+      OrderItem.create(item_id: item_2.id, order_id: order_1.id, quantity: 3, current_price: 20.00, fulfilled: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit profile_order_path(order_1)
+
+      click_button 'Cancel Order'
+
+      expect(current_path).to eq(profile_order_path(order_1))
+      expect(page).to_not have_button('Cancel Order')
+      expect(page).to have_content("Status: Cancelled")
+    end
+  end
 end
