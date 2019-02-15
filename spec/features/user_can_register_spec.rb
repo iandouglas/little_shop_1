62ccc996f305897d7f488657e7c6562d3478bcd2
vjp888 <rellik888@gmail.com>
2019-02-14 17:19:04 -0700

    Adjusts tests to test for email errors and redirect back to the edit path

--- spec/features/user_can_register_spec.rb
+++ spec/features/user_can_register_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'as visitor', type: :feature do
     fill_in 'Zip code', with: 80216
     fill_in 'Email', with: 'bob@bob.net'
     fill_in 'Password', with: 'password'
-    fill_in 'Confirm password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
     click_on 'Register Now'
     user = User.last
 
@@ -34,7 +34,7 @@ RSpec.describe 'as visitor', type: :feature do
     fill_in 'State', with: 'CO'
     fill_in 'Email', with: 'bob@bob.net'
     fill_in 'Password', with: 'password'
-    fill_in 'Confirm password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
     click_on 'Register Now'
 
     expect(page).to have_content("You are missing information")
@@ -55,7 +55,7 @@ RSpec.describe 'as visitor', type: :feature do
     fill_in 'Zip code', with: 80216
     fill_in 'Email', with: 'bob@bob.net'
     fill_in 'Password', with: 'password'
-    fill_in 'Confirm password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
     click_on 'Register Now'
 
     expect(page).to have_content('This E-mail is already registered')
