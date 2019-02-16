f0f4170107067bbf473e1b707b5f1435d95331e2
vjp888 <rellik888@gmail.com>
2019-02-16 14:24:30 -0700

    Adds test for admin user show page

---
+++ spec/features/admin_user_information_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'As an admin', type: :feature do
+
+  it 'lets an admin view a user showpage' do
+    admin = User.create!(username: 'test', street: '123 main st', city: 'denver', state: 'CO', zip_code: 80216, email: 'test@bob.net', password: 'password', role: 2)
+    user = User.create!(username: 'happy', street: '432 main st', city: 'steve', state: 'CO', zip_code: 80126, email: 'te@bob.net', password: 'password', role: 0)
+
+    visit admin_user_show_path(user)
+
+    expect(page).to have_content("Name: #{user.username}")
+    expect(page).to have_content("Street Address: #{user.street}")
+    expect(page).to have_content("City: #{user.city}")
+    expect(page).to have_content("state: #{user.state}")
+    expect(page).to have_content("Zip Code: #{user.zip_code}")
+    expect(page).to have_content("Email: #{user.email}")
+    expect(page).to link_to("Edit Profile")
+  end
+
+
+end
