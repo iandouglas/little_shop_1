902dd2d0d51d7157243d2a1f1ae7327a7db6fc36
Rene Casco <rene.casco@icloud.com>
2019-02-17 02:30:07 -0700

    model test passing

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :order_items
   has_many :orders, through: :order_items
+
+  validates_presence_of :name, :description, :quantity, :price, :thumbnail, :enabled, :user_id
 end
