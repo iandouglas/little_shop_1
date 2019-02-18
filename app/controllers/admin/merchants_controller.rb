2691f551da082e462502f595fea77bc2a6745623
Noah Flint <noah.flint24@gmail.com>
2019-02-18 12:26:17 -0700

    created disable method for admin/merchants_controller

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -4,4 +4,24 @@ class Admin::MerchantsController < ApplicationController
   def show
     @merchant = User.find(params[:id])
   end
+
+  def disable
+    user = User.find(params[:id])
+    if user.enabled?
+      user.enabled = 'disabled'
+      user.save
+    end
+    flash[:success] = "#{user.username} is now disabled"
+    redirect_to merchants_path
+  end
+
+  # def enable
+  #   user = User.find(params[:id])
+  #   if user.disabled?
+  #     user.enabled = 'enabled'
+  #     user.save
+  #   end
+  #   flash[:success] = "#{user.username} is now enabled"
+  #   redirect_to admin_users_path
+  # end
 end
