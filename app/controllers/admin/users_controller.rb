c0960ffecdf112f07807ace8f2a62b2a829d1bfb
Noah Flint <noah.flint24@gmail.com>
2019-02-18 12:26:17 -0700

    created enable method in sessions controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -41,8 +41,17 @@ before_action :require_admin
       user.enabled = 'disabled'
       user.save
     end
-
     flash[:success] = "#{user.username} is now disabled"
+    redirect_to admin_users_path
+  end
+
+  def enable
+    user = User.find(params[:id])
+    if user.disabled?
+      user.enabled = 'enabled'
+      user.save
+    end
+    flash[:success] = "#{user.username} is now enabled"
     redirect_to admin_users_path
   end
 
