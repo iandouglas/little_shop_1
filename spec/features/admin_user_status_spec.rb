9cd52aff301dbda8089132fd0aac23fd80ea7999
Noah Flint <noah.flint24@gmail.com>
2019-02-18 12:26:17 -0700

    refactored test to user helper methods to login/logout, changed path for enabled and disabled

--- spec/features/admin_user_status_spec.rb
+++ spec/features/admin_user_status_spec.rb
@@ -6,26 +6,19 @@ RSpec.describe 'As an Admin', type: :feature do
     admin = User.create!(username: 'test', street: '123 main st', city: 'denver', state: 'CO', zip_code: 80216, email: 'test@bob.net', password: 'password', role: 2)
     user_1 = User.create!(username: 'happy', street: '432 main st', city: 'steve', state: 'CO', zip_code: 80126, email: '1@bob.net', password: 'password', role: 0)
     user_2 = User.create!(username: 'nothappy', street: '432 main st', city: 'steve', state: 'CO', zip_code: 80126, email: '2@bob.net', password: 'password', role: 0)
-    visit login_path
-    fill_in 'Email', with: 'test@bob.net'
-    fill_in 'Password', with: 'password'
-    click_button 'Sign In'
+    login_as(admin)
 
     visit admin_users_path
 
-    within "#user-#{user_1.id}" do
+    within ".user-#{user_1.id}" do
       click_button "Disable"
     end
 
-    expect(path).to eq(admin_users_path)
+    expect(current_path).to eq(admin_users_path)
 
-    click_link 'Logout'
-    click_link 'Log In'
-    fill_in 'Email', with: '1@bob.net'
-    fill_in 'Password', with: 'password'
-    click_button 'Sign In'
+    login_as(user_1)
 
     expect(current_path).to eq(login_path)
   end
-  
+
 end
