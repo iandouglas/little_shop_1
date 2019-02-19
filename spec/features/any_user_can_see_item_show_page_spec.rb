3f544f7d298cc07b7585be4ceb338a34a501e250
Rene Casco <rene.casco@icloud.com>
2019-02-19 11:01:12 -0700

    initial commit for item index page tests

---
+++ spec/features/any_user_can_see_item_show_page_spec.rb
@@ -0,0 +1,9 @@
+equire 'rails_helper'
+
+RSpec.describe "When I visit an item's show page from the items catalog", type: :feature do
+  describe 'as visitor' do
+    it 'lets me see all information for this item' do
+
+    end
+  end
+end
