b2510934bc53ad6bc4fa6451da15385930796dad
Ty Mazey <tymazey@gmail.com>
2019-02-20 13:18:55 -0700

    adds merchant index test and view

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -3,6 +3,5 @@ class Merchants::ItemsController < ApplicationController
   def index
     @items = Item.where(user_id: current_user.id)
     @unsold_items = Item.unsold_items(@items)
-    binding.pry
   end
 end
